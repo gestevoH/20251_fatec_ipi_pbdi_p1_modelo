b8357436e761141a0a2cb9bc7c81f803e9977cc6
Gustavo Henrique <gustavosousasantos54@gmail.com>
2025-04-29 11:20:04 -0300

    feat(p1): encontra aprovados que estudam sozinhos

--- script.sql
+++ script.sql
@@ -19,32 +19,51 @@
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
 
-DO $$ 
-DECLARE 
-    cur_phd_pais REFCURSOR; 
-    student_id VARCHAR(200); 
-    father_edu INT; 
-    mother_edu INT; 
-    grade INT;
-BEGIN 
-    OPEN cur_phd_pais FOR 
-    SELECT student_prediction.student_id, student_prediction.grade, student_prediction.father_edu, student_prediction.mother_edu 
-    FROM student_prediction;
-    LOOP 
-        FETCH cur_phd_pais INTO student_id, grade, father_edu, mother_edu;
-        EXIT WHEN NOT FOUND;
-        IF (father_edu = 6 OR mother_edu = 6) AND grade <> 0 THEN 
-		    RAISE NOTICE 'alunos com pais com phd e aprovados: %', student_id;
-        END IF;
-    END LOOP;
-    CLOSE cur_phd_pais;
-END; 
-$$
+-- DO $$ 
+-- DECLARE 
+--     cur_phd_pais REFCURSOR; 
+--     student_id VARCHAR(200); 
+--     father_edu INT; 
+--     mother_edu INT; 
+--     grade INT;
+-- BEGIN 
+--     OPEN cur_phd_pais FOR 
+--     SELECT student_prediction.student_id, student_prediction.grade, student_prediction.father_edu, student_prediction.mother_edu 
+--     FROM student_prediction;
+--     LOOP 
+--         FETCH cur_phd_pais INTO student_id, grade, father_edu, mother_edu;
+--         EXIT WHEN NOT FOUND;
+--         IF (father_edu = 6 OR mother_edu = 6) AND grade <> 0 THEN 
+-- 		    RAISE NOTICE 'alunos com pais com phd e aprovados: %', student_id;
+--         END IF;
+--     END LOOP;
+--     CLOSE cur_phd_pais;
+-- END; 
+-- $$
 
 -- ----------------------------------------------------------------
 -- 3 Resultado em função dos estudos
 --escreva a sua solução aqui
 
+DO $$
+DECLARE
+    cur_aprov_so REFCURSOR;
+    qtd_aprov INT;
+    tabela VARCHAR(100) := 'student_prediction';
+BEGIN
+    OPEN cur_aprov_so FOR EXECUTE format(
+	        'SELECT COUNT(*) FROM %I WHERE grade >= 6 AND prep_study = 1',
+	        tabela
+	    );
+	    FETCH cur_aprov_so INTO qtd_aprov;
+    CLOSE cur_aprov_so;
+    IF qtd_aprov = 0 THEN
+        RAISE NOTICE '%', -1;
+    ELSE
+        RAISE NOTICE 'Resultado: %', qtd_aprov;
+    END IF;
+END 
+$$;
 
 -- ----------------------------------------------------------------
 -- 4 Salário versus estudos
