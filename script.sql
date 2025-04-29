eb4284e15ec5d2ecce6d48ee62702567d2496fda
Gustavo Henrique <gustavosousasantos54@gmail.com>
2025-04-29 10:42:51 -0300

    feat(p1): cria base e importa dados

--- script.sql
+++ script.sql
@@ -2,7 +2,19 @@
 -- 1 Base de dados e criação de tabela
 --escreva a sua solução aqui
 
+CREATE TABLE student_prediction(
+	student_id SERIAL PRIMARY KEY,
+	grade INT,
+	father_edu INT,
+	mother_edu INT,
+	prep_study INT,
+	prep_exam INT,
+	salary INT
+);
 
+ALTER TABLE student_prediction ALTER COLUMN student_id TYPE VARCHAR(200);
+
+SELECT * FROM student_prediction;
 -- ----------------------------------------------------------------
 -- 2 Resultado em função da formação dos pais
 --escreva a sua solução aqui
